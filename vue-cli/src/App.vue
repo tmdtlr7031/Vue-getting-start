8001da270cc293f92f7b8d4477262c48fca8718b
seungsik <seungsik@9bit.co.kr>
2023-01-10 16:15:26 +0900

    싱글파일 컴포넌트에서 props 사용

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <!-- 최상위 요소는 하나만 있어야함 (같은 레벨로 복수개 불가)-->
   <div>
-    {{ str }}
+    <app-header v-bind:prosdata="str"></app-header>
   </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue';
+
 export default {
   data: function() {
     return {
-      str : 'hi'
+      str : 'Header'
     }
+  },
+  components: {
+    'app-header' : AppHeader
   }
 }
 </script>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <h1>{{ prosdata }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+    props: ['prosdata']
+}
+</script>
+
+<style>
+
+</style>
