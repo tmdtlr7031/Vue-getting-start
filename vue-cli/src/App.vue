dd4e762cdad0f675a6f65f1dd897460d64512bef
seungsik <seungsik@9bit.co.kr>
2023-01-10 15:21:13 +0900

    싱글파일 컨포넌트로 개발 시작하기

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- 최상위 요소는 하나만 있어야함 (같은 레벨로 복수개 불가)-->
+  <div>
+    {{ str }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      str : 'hi'
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
